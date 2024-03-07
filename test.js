f0593f5be08cd219566f804ada59cd42d83ab8f8
Yusuke0620 <boston19851229@gmail.com>
2024-03-07 18:08:51 +0900

    Edited: Coding completed for now.

--- test.js
+++ test.js
@@ -1,22 +1,12 @@
-const ball = document.querySelector('.ball');
-
-//クリックイベント
-ball.addEventListener('click', () => {
-
-    //キーフレーム//透明から不透明
-    const keyframes = [
-        { opacity: 0.1, transform: 'translate(0, 0)' },
-        { opacity: 1, transform: 'translate(300px, 0)' },
-    ];
-    //オプション
-    const options = {
-        duration: 1000,
-        easing: 'ease-in-out',
-    };
-    
-    //animateで出力
-    ball.animate(keyframes, options);
-});
 
 
-
+(function() {
+    const fh = document.getElementById('fixed-header');
+    window.addEventListener('scroll', () => {
+      if (window.pageYOffset > 100) {
+        fh.classList.add('is-show');
+      } else {
+        fh.classList.remove('is-show');
+      }
+    });
+}());
