71ad8676ba626e204186aac841190dfd54d100a6
Yusuke0620 <boston19851229@gmail.com>
2024-03-19 21:14:35 +0900

    Study JavaScript

--- test.js
+++ test.js
@@ -1,12 +1,40 @@
+/*-------------------------------------------
+要素をスクロールで表示
+-------------------------------------------*/
+
+const animateFade = (entries, obs) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log(entry.target);
+
+      entry.target.animate(
+        {
+          opacity: [0, 1],
+          filter: ['blur(.4rem)', 'blur(0)'],
+          translate: ['0 4rem', 0],
+        },
+        {
+          duration: 2000,
+          easing: 'ease',
+          fill: 'forwards',
+        }
+      );
+
+      obs.unobserve(entry.target);
+    }
+  });
+};
 
 
-(function() {
-    const fh = document.getElementById('fixed-header');
-    window.addEventListener('scroll', () => {
-      if (window.pageYOffset > 100) {
-        fh.classList.add('is-show');
-      } else {
-        fh.classList.remove('is-show');
-      }
-    });
-}());
+
+
+
+//監視ロボ
+const fadeObserver = new IntersectionObserver(animateFade);
+//監視する要素を代入しておく
+const fadeElements = document.querySelectorAll('.fadein');
+
+fadeElements.forEach((fadeElement) => {
+  //監視要素を指示
+  fadeObserver.observe(fadeElement);
+})
